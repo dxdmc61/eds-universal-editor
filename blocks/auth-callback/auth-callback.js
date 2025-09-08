eaf62cd2dfb0d7f18b689a0665235d73f598544c
Jaganath Panda <jaganathpanda@Jaganaths-MacBook-Air.local>
2025-09-08 15:07:50 +0530

    login block

---
+++ blocks/auth-callback/auth-callback.js
@@ -0,0 +1,14 @@
+export default function decorate(block) {
+    const hash = window.location.hash.substring(1);
+    const params = new URLSearchParams(hash);
+    const token = params.get('access_token');
+  
+    if (token) {
+      localStorage.setItem('ims_token', token);
+      block.innerHTML = '<p>Login successful! Redirecting...</p>';
+      setTimeout(() => window.location.href = '/', 1000);
+    } else {
+      block.innerHTML = '<p>Login failed. Please try again.</p>';
+    }
+  }
+  
